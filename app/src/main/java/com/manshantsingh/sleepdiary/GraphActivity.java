720efa00889ae5dbd3707b9e6cb86222b22b8e09
Manshant Singh Kohli <me@manshantsingh.com>
2017-09-17 01:05:05 -0700

    chart framework setup for display but no data

--- app/src/main/java/com/manshantsingh/sleepdiary/GraphActivity.java
+++ app/src/main/java/com/manshantsingh/sleepdiary/GraphActivity.java
@@ -1,13 +1,48 @@
 package com.manshantsingh.sleepdiary;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class GraphActivity extends Activity {
+
+    LineChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+        chart = (LineChart) findViewById(R.id.chart1);
+
+        // TODO: get data from database and set it into the correct format
+        ArrayList<Entry> plotData = new ArrayList<Entry>();
+
+        LineDataSet set1 = new LineDataSet(plotData, "DataSet 1");
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set1.setColor(ColorTemplate.getHoloBlue());
+        set1.setValueTextColor(ColorTemplate.getHoloBlue());
+        set1.setLineWidth(1.5f);
+        set1.setDrawCircles(false);
+        set1.setDrawValues(false);
+        set1.setFillAlpha(65);
+        set1.setFillColor(ColorTemplate.getHoloBlue());
+        set1.setHighLightColor(Color.rgb(244, 117, 117));
+        set1.setDrawCircleHole(false);
+
+        LineData data = new LineData(set1);
+        data.setValueTextColor(Color.WHITE);
+        data.setValueTextSize(9f);
+
+        chart.setData(data);
     }
 }
